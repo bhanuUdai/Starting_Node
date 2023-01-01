8f62dc1f282a212023df05962883810d8dc5cfc3
bhanu <udaibhanu2602@gmail.com>
2023-01-01 21:40:59 +0530

    Request/Response

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const server=http.createServer(function(req,res)
         </head>
         <body>
             <h1>Hello Bhanu</h1>
-            <form action='/about' >
+            <form action='/about' method='POST' >
                 <button type='submit' >ABOUT PAGE</button>
             </form>
         </body>
@@ -30,9 +30,9 @@ const server=http.createServer(function(req,res)
     return res.end() // res.end() tells the node that our code ends here, below this if we agin write res.write(), it will give error,also here we are using return because here we want to stop the code and it will not execute further below. Otherwise it will also execute content of another url
     }
     
-    if(req.url==='/about?')
+    if(req.url==='/about')
     {
-        res.setHeader('Content-Type','text/html');
+       // res.setHeader('Content-Type','text/html');
         res.write(`<html>
             <head>
                 <title>
@@ -42,7 +42,7 @@ const server=http.createServer(function(req,res)
             <body>
                 <h1>Hello Bhanu</h1>
                 <p> Welcome to about page</p>
-                <form action='/node' >
+                <form action='/node' method='POST' >
                     <button type='submit' >NODE PAGE</button>
                 </form>
             </body>
